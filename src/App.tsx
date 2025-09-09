f48df3f1f9a79996ad7c38e4bfa12ad65c2ae5cb
shrawan <shrawan29yadav@gmail.com>
2025-09-09 06:58:49 +0530

    frntend started

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Landing } from './screens/Landing'
+import { Game } from './screens/Game'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen bg-[rgba(135,95,78,1)]'>
+        <BrowserRouter>
+          <Routes>
+
+            <Route path='/landing' element={<Landing />} />
+            <Route path='/game' element={<Game />} />
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
